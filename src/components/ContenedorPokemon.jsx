5394edc51cb511a6f5918bc203d19251bed8a846
Informatica <informatica@alumno.local>
2024-08-30 17:24:29 -0600

    Cambios en Clase

--- src/components/ContenedorPokemon.jsx
+++ src/components/ContenedorPokemon.jsx
@@ -4,26 +4,30 @@ import { usePokemon } from "../hooks/usePokemon"
 export const ContenedorPokemon = ({infoPoke}) => {
   return (
     <>
-      <div>
+      <div className="d-flex flex-row justify-content-center alig-items-center row row-cols-2 p-2">
       {
         infoPoke.map(({nombre,imagen,estadisticas,habilidades})=>{
-            <div className="card" style="width: 18rem;">
+            return(
+                <div className="card" style="width: 18rem;">
             <img src={imagen} alt="" />
             <div className="card-body">
                 <h5 className="card-title">Nombre</h5>
                 <p className="card-text">{nombre}</p>
             </div>
             <ul class="list-group list-group-flush">
-                habilidades
-                <li className="list-group-item">{{}}</li>
-                <li className="list-group-item">A second item</li>
-                <li className="list-group-item">A third item</li>
+                {
+                    estadisticas.map(({nombre,base})=>{
+                        <li className="list-group-item">{nombre}{base}</li>
+                        
+                    })
+                }
             </ul>
             <div className="card-body">
                 <a href="#" className="card-link">Card link</a>
                 <a href="#" className="card-link">Another link</a>
             </div>
             </div>
+            )
         })
       }
       </div>
